9246f5864492bfff3c020f84e8d2e8b712329fa7
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-16 22:20:32 +0900

    특수 구분자 뺴고 기본 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,10 +7,10 @@ public class Application {
     public static void main(String[] args) {
 
         try{
-            // 원래는 try-catch가 없었음 : 예외처리로 추가함, 예외 throw하니까 그냥 뭔가 잘못 끝나는 느낌
-            System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+            System.out.println("input ");
             String str = Console.readLine();
-            // 입력한 거 check
+
             System.out.println("str = " + str);
 
             int result = 0;
@@ -19,20 +19,24 @@ public class Application {
             char c1 = str.charAt(0);
             char c2 = str.charAt(str.length()-1);
 
-            // 문자열 형식으로 들어왔는지 체크
+            // check for format is right
             if (c1=='\"' && c2=='\"'){
-                // 따옴표 제외 문자열 추출
+                // get string without " "
                 String substring = str.substring(1, str.length() - 1);
-                // 구분자를 기준으로 나누고 정수로 변환
-                String[] split = substring.split(": | ,");
+                // change to integer
+                String[] split = substring.split(":|,");
                 System.out.println("split = " + Arrays.toString(split));
-                System.out.println("결과 : "+ result);
+                for (String s : split) {
+                    int s1 = Integer.parseInt(s);
+                    result +=s1;
+                }
+                System.out.println("result : "+ result);
             }
-            else throw new IllegalStateException("잘못된 값을 입력하였습니다.");
+            else throw new IllegalStateException("IllegalStateException : write a wrong input");
         }
-        // 문자열 형식으로 들어오지 않으면 IllegalArgumentException
+        // if format is wrong sout IllegalArgumentException
         catch (IllegalStateException e){
-
+            System.out.println(e.getMessage());
         }
 
 
