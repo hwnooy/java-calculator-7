798a006946662481415886a795f1aff97ad2435d
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-16 21:55:53 +0900

    일단 예외처리 없이 코드 짜기 -> 예외처리 넣기 -> try catch 넣음 -> 잘못된 입력에 대해 else에 예외 throw 던짐 추가 -> 까지 진행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,44 @@
 package calculator;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
-        
-        String input = Console.readLine();
-        System.out.println("input = " + input);
+
+        try{
+            // 원래는 try-catch가 없었음 : 예외처리로 추가함, 예외 throw하니까 그냥 뭔가 잘못 끝나는 느낌
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String str = Console.readLine();
+            // 입력한 거 check
+            System.out.println("str = " + str);
+
+            int result = 0;
+            if (str=="") result = 0;
+
+            char c1 = str.charAt(0);
+            char c2 = str.charAt(str.length()-1);
+
+            // 문자열 형식으로 들어왔는지 체크
+            if (c1=='\"' && c2=='\"'){
+                // 따옴표 제외 문자열 추출
+                String substring = str.substring(1, str.length() - 1);
+                // 구분자를 기준으로 나누고 정수로 변환
+                String[] split = substring.split(": | ,");
+                System.out.println("split = " + Arrays.toString(split));
+                System.out.println("결과 : "+ result);
+            }
+            else throw new IllegalStateException("잘못된 값을 입력하였습니다.");
+        }
+        // 문자열 형식으로 들어오지 않으면 IllegalArgumentException
+        catch (IllegalStateException e){
+
+        }
+
+
+
+
+
+
     }
 }
