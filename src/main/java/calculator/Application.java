dce1462541b6723827e255152757ae681c00cc72
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-16 23:38:43 +0900

    특수문자 구분자 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -21,14 +21,34 @@ public class Application {
 
             // check for format is right
             if (c1=='\"' && c2=='\"'){
-                // get string without " "
-                String substring = str.substring(1, str.length() - 1);
+                // get string without " ", last is number
+                String sub = str.substring(1, str.length() - 1);
                 // change to integer
-                if (){
-                    // check for what is between // and \n
+                if (str.charAt(1)=='/' && str.charAt(2)=='/'){
+                    System.out.println("test for special pivot");
+                    System.out.println("index of  target : " +  str.indexOf("\\n"));  // \r\n(x) ascii 10(X)
+
+                    String pivot = str.substring(3, str.indexOf("\\n"));
+                    System.out.println("pivot is "+pivot);
+                    System.out.println(str.indexOf("\\n"));
+                    System.out.println(str.charAt(str.indexOf("\\n")));
+                    // after \n
+                    System.out.println("print target");
+                    String target = str.substring((str.indexOf("\\n")+2), str.length()-1); // is \n not a single char??
+                    System.out.println("target : " + target);
+
+                    String[] split = target.split(pivot);
+                    System.out.println("split = " + Arrays.toString(split));
+
+                    for (String s : split) {
+                        int s1 = Integer.parseInt(s);
+                        result +=s1;
+                    }
+                    System.out.println("result : "+ result);
+
                 }
                 else    {
-                    String[] split = substring.split(":|,");
+                    String[] split = sub.split(":|,");
                     System.out.println("split = " + Arrays.toString(split));
                     for (String s : split) {
                         int s1 = Integer.parseInt(s);
